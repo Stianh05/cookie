06bc1ae88823cdc47cb55f29bcd2fe63aa7f27b3
Stianh05 <stian.haveland@icloud.com>
2023-03-22 10:31:14 +0100

    hei

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
-const express = require("express");
-const sqlite3 = require('better-sqlite3')
-const db = sqlite3('coockie-clicker.db')
-const path = require('path')
-const hbs = require('hbs')
-const session = require('express-session');
-const { request } = require("http");
-const app = express();
-const bcrypt = require('bcrypt');
+// Importerer nødvendige biblioteker og oppretter en express-app
+const express = require("express"); 
+const sqlite3 = require('better-sqlite3') // Bedre versjon av SQLite
+const db = sqlite3('coockie-clicker.db') // Oppretter en database-tilkobling
+const path = require('path') // Hjelpebibliotek for å håndtere filstier
+const hbs = require('hbs') // Hjelpebibliotek for å generere HTML-templates
+const session = require('express-session'); // Bibliotek for å håndtere sesjoner
+const bcrypt = require('bcrypt'); // Bibliotek for å hashe passord
+const app = express(); // Oppretter en ny express-app
 
+// Setter opp middleware for statiske filer og form-data
 const publicDirectoryPath = path.join(__dirname, "/public")
 app.use(express.static(publicDirectoryPath))
 app.use(express.urlencoded({ extended: true }));
 app.use("/savegame",express.json());
 
+// Setter opp templating med Handlebars
 const viewPath = path.join(__dirname, "/views")
 app.set("view engine", hbs)
 app.set('views',viewPath)
 
-
+// Setter opp sesjoner
 app.use(session({
-    secret: "Keep it secret",
-    resave: false,
-    saveUninitialized: false
+    secret: "Keep it secret", // hemmelig nøkkel brukt for å signere sesjonsdata
+    resave: false, // krever ikke å lagre sesjonsdata på hver request
+    saveUninitialized: false // krever ikke å opprette en ny sesjon for hver request
 }));
 
-
+// Initialiserer variabler for bruker og bruker-ID
 let Usernow = ""
 let UserIdNow = ""
 
@@ -36,7 +38,7 @@ app.get("/brukere", (request, response) => {
 })
 */
 
-
+// Rute for brukersiden
 app.get("/acount", (request, response) => {
     if (request.session.logedIn == true) {
         const sql = db.prepare("SELECT * FROM User WHERE id=(?)")
@@ -52,63 +54,67 @@ app.get("/acount", (request, response) => {
         })
     }
     else {
-        response.redirect("http://localhost:3000/login%20and%20register.html")
+        response.redirect("http://localhost:3000/login_and_register.html")
     }
 })
 
-
+// Rute for å legge til ny bruker
 app.post("/addUser", (request, response) => {
-    const username = request.body.name
-
-    const sql = db.prepare("SELECT * FROM User WHERE name=(?)")
-    const userRow = sql.get(username)
-    
-    if (userRow == undefined){ //bruker finnes ikke, lager ny
-        const sql = db.prepare('INSERT INTO User (name,password,savedata) VALUES (?,?,?)')
-        const hashedPassword = bcrypt.hashSync(request.body.password, 10)
-        const info = sql.run(request.body.name, hashedPassword, request.body.savedata)
-
-        Usernow = username
-        
-        const tbl = db.prepare("SELECT * FROM User WHERE name=(?)")//hent id fra ny bruker
-        const User = tbl.get(username)
-        UserIdNow = User.id
-
-        request.session.logedIn = true
-        response.redirect("/")
-    }
-})
-
-
-app.post("/login", (request, response) => {
-    const sql = db.prepare("SELECT * FROM User WHERE name=(?)")
-
-    const username = request.body.name
-    const password = request.body.password
+    const username = request.body.name;
   
-    const userRow = sql.get(username)
-    const isCorrect  = bcrypt.compareSync(password, userRow.password);
-   
+    // Sjekk om brukeren allerede eksisterer i databasen
+    const sql = db.prepare("SELECT * FROM User WHERE name=(?)");
+    const userRow = sql.get(username);
   
-    if (isCorrect == true){
-    Usernow = request.body.name
-    UserIdNow = userRow.id
-    request.session.logedIn = true
-    response.redirect("http://localhost:3000")
+    if (userRow == undefined) {
+      // Hvis brukeren ikke finnes, legg den til i databasen
+      const sql = db.prepare("INSERT INTO User (name,password,savedata) VALUES (?,?,?)");
+      const hashedPassword = bcrypt.hashSync(request.body.password, 10); // Hasjer passordet før det lagres
+      const info = sql.run(request.body.name, hashedPassword, request.body.savedata);
+  
+      Usernow = username;
+  
+      // Hent ID fra ny bruker
+      const tbl = db.prepare("SELECT * FROM User WHERE name=(?)");
+      const User = tbl.get(username);
+      UserIdNow = User.id;
+  
+      // Sett logget inn status for brukeren til true
+      request.session.logedIn = true;
+      response.redirect("/");
     }
-    else {
-        request.session.logedIn = false
-        response.redirect("back")
+  });
+  
+  // Rute for å logge inn en bruker
+  app.post("/login", (request, response) => {
+    const sql = db.prepare("SELECT * FROM User WHERE name=(?)");
+  
+    const username = request.body.name;
+    const password = request.body.password;
+  
+    const userRow = sql.get(username);
+    const isCorrect = bcrypt.compareSync(password, userRow.password); // Sjekk om passordet er korrekt
+  
+    if (isCorrect == true) {
+      // Sett brukeren som logget inn og lagre brukerinfo i sessions
+      Usernow = request.body.name;
+      UserIdNow = userRow.id;
+      request.session.logedIn = true;
+      response.redirect("/");
+    } else {
+      // Hvis innloggingen ikke er korrekt, sett logget inn status til false og redirect tilbake til forrige side
+      request.session.logedIn = false;
+      response.redirect("back");
     }
-})
-
-
-app.get("/logout", (request, response) => {
-    Usernow = ""
-    UserIdNow = ""
-    request.session.logedIn = false
-    response.redirect("http://localhost:3000/login%20and%20register.html")
-})
+  });
+  
+  // Rute for å logge ut en bruker
+  app.get("/logout", (request, response) => {
+    Usernow = "";
+    UserIdNow = "";
+    request.session.logedIn = false;
+    response.redirect("http://localhost:3000/login_and_register.html");
+  });
 
 
 app.get('/delete', (request, response) => {
@@ -116,7 +122,7 @@ app.get('/delete', (request, response) => {
     tbl_User_Pokal.run(request.query.id)
     const sql = db.prepare('DELETE FROM User WHERE id=(?)')
     sql.run(request.query.id)
-    response.redirect("http://localhost:3000/login%20and%20register.html")
+    response.redirect("http://localhost:3000/login_and_register.html")
 })
 
 
